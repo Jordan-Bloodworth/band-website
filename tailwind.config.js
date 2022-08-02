3b7b8fb1a0390a6d2efe68d0c81fa52a118d2d06
Jordan Bloodworth <98141686+Jordan-Bloodworth@users.noreply.github.com>
2022-08-02 18:45:44 -0500

    navbar setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  theme: {
+		fontFamily: {
+			proda: ["ProdaSans-Regular", "sans-serif"],
+      boba: ["Boba-Milk", "sans-serif"],
+		},
+    extend: {},
+  },
+  plugins: [],
+}
